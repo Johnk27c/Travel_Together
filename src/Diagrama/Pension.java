5d0929d644d0daf73102caa62a0a98120c117334
francoherreraa <francoherrera654@gmail.com>
2024-10-22 19:21:57 -0300

    Clases Turista y Pension

---
+++ src/Diagrama/Pension.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Diagrama;
+
+/**
+ *
+ * @author Franco
+ */
+public class Pension {   
+    private int codAdicional;
+    private String nombre;
+    private double porcentaje;
+            
+}
---
+++ src/Diagrama/Turista.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Diagrama;
+
+/**
+ *
+ * @author Franco
+ */
+public class Turista {   
+    private int dni;
+    private int nombre;
+    private int edad;
+    
+}
