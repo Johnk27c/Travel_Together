aac872829323335ebc86de2dafa86b2a4afff6a9
santiagolara01 <santilara10@gmail.com>
2024-11-01 21:16:45 -0300

    algo

--- src/Persistencia/PasajeData.java
+++ src/Persistencia/PasajeData.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Persistencia;
 
+import Entidades.Pasaje;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
- *
  * @author Santiago Lara
  */
 public class PasajeData {
     
+    private Connection conexion = null;
+    
+    public  PasajeData(){
+        conexion = Conexion.getConexion();
+    }
+    
+    public void buscarPasaje(int codPasaje){
+        String sql = "SELECT codPasaje, fechaHora, idCiudad_Origen, idCiudad_Destino, asiento, transporte, precio "
+                + "FROM pasaje WHERE codPasaje = ?";
+        Pasaje pasaje = null;
+        
+        try {
+            PreparedStatement ps = conexion.prepareStatement(sql);
+            ps.setInt(1, codPasaje);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                pasaje = new Pasaje();
+                pasaje.setCodPasaje(rs.getInt("codPasaje"));
+                
+                
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a tabla pasaje");
+        }
+    }
+    
+    public void modificarPasaje(){
+        String sql = "INSERT into pasaje ()";
+    }
+    public void eliminarPasaje(){
+        String sql = "DELETE FROM pasaje WHERE codPasaje = ?";
+        
+        try {
+            PreparedStatement ps =conexion.prepareStatement(sql);
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a tabla pasaje");
+        }
+                
+    }
 }
